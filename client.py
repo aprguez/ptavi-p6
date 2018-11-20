487bcfc6a1823b1226ce5e306f5d7c7ccc3e614a
aprguez <alejandro_p-rguez@hotmail.com>
2018-11-20 23:56:29 +0100

    Final sin pep8

--- client.py
+++ client.py
@@ -5,12 +5,14 @@
 import socket
 import sys
 
+#Cliente UDP simple.
 if len(sys.argv) != 3:
     sys.exit('Usage: python client.py method receiver@IP:SIPport')
-
+    
 METODO = sys.argv[1]
 DIRECCION = sys.argv[2].split('@')
-# Login, Dirección IP, Puerto del servidor.
+
+# Dirección IP del servidor.
 LOGIN = DIRECCION[0]
 IP = DIRECCION[1].split(':')[0]
 PORT = int(DIRECCION[1].split(':')[-1])
@@ -20,12 +22,20 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((IP, PORT))
 
-# Contenido que vamos a enviar
+# Contenido que vamos a enviar.
 LINE = METODO + ' sip:' + LOGIN + '@' + IP + ' SIP/2.0\r\n'
+my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+print('Enviando: ' + LINE)
 
-print("Enviando: " + LINE)
-my_socket.send(LINE)
+# Contenido que recibimos de respuesta
+data = my_socket.recv(1024)
+print('Recibido -- ', data.decode('utf-8'))
 
-# Cerramos todo
+if METODO == 'INVITE':
+    LINE = 'ACK sip:' + LOGIN + '@' + IP + ' SIP/2.0\r\n'
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print('Enviando: ' + LINE)
+
+#Cerramos.
 my_socket.close()
 print("Fin.")
