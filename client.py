642d0f3ce61cb472512a562270366c6d1d435cbf
aprguez <alejandro_p-rguez@hotmail.com>
2018-11-20 23:19:02 +0100

    Prueba

--- client.py
+++ client.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
+"""Programa cliente SIP."""
 
 import socket
+import sys
 
-# Cliente UDP simple.
+if len(sys.argv) != 3:
+    sys.exit('Usage: python client.py method receiver@IP:SIPport')
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+METODO = sys.argv[1]
+DIRECCION = sys.argv[2].split('@')
+# Login, Dirección IP, Puerto del servidor.
+LOGIN = DIRECCION[0]
+IP = DIRECCION[1].split(':')[0]
+PORT = int(DIRECCION[1].split(':')[-1])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((IP, PORT))
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
+# Contenido que vamos a enviar
+LINE = METODO + ' sip:' + LOGIN + '@' + IP + ' SIP/2.0\r\n'
 
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
+print("Enviando: " + LINE)
+my_socket.send(LINE)
 
+# Cerramos todo
+my_socket.close()
 print("Fin.")
-
